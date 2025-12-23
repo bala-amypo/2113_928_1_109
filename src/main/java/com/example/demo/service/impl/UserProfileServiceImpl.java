780f5124bac8be402272a87b1bda47cac00fa507
bala-amypo <bala@amypo.in>
2025-12-23 08:47:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserProfileServiceImpl.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.UserProfileEntity;
@@ -5,12 +6,10 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserProfileRepository;
 import com.example.demo.service.UserProfileService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
 public class UserProfileServiceImpl implements UserProfileService {
 
     private final UserProfileRepository repository;
@@ -25,15 +24,12 @@ public class UserProfileServiceImpl implements UserProfileService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public UserProfileEntity getUserById(Long id) {
         return repository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("User not found with id " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<UserProfileEntity> getAllUsers() {
         return repository.findAll();
     }
